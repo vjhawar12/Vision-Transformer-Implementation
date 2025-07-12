3395c368fa1ff56f923c84c56713c9c5842d0c55
Vedant Jhawar <89154560+vjhawar12@users.noreply.github.com>
2025-07-12 02:58:01 +0000

    Update test_utils.py

--- vit/test_utils.py
+++ vit/test_utils.py
@@ -7,7 +7,10 @@ def test(vit, test_dataloader, device):
         for image, label in test_dataloader:
             image, label = image.to(device), label.to(device)
 
-            with torch.amp.autocast(device_type="cuda"):
+            if torch.cuda.is_available():
+                with torch.amp.autocast(device_type="cuda"):
+                    output = vit(image)
+            else:
                 output = vit(image)
 
             pred = torch.argmax(output, dim=1)
