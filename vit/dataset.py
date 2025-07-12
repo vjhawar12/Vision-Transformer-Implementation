9bd6eaa0179c7a1a89ff27d5abfb925d18d78f4c
Vedant Jhawar <89154560+vjhawar12@users.noreply.github.com>
2025-07-12 10:03:43 +0000

    Update dataset.py

--- vit/dataset.py
+++ vit/dataset.py
@@ -1,9 +1,11 @@
 from torchvision import datasets
 
+""" Applies a train_transform and returns a training dataset """
 def get_training_data(train_transform):
     training_data = datasets.CIFAR10(root="data", train=True, download=True, transform=train_transform)
     return training_data
 
+""" Applies a test_transform and returns a testing dataset """
 def get_test_data(test_transform):
     test_data = datasets.CIFAR10(root="data", train=False, download=False,  transform=test_transform)
     return test_data
