f9437a82d2fa2fcbf9886a2f307ae30cfc0bc861
Vedant Jhawar <89154560+vjhawar12@users.noreply.github.com>
2025-07-12 10:02:28 +0000

    Update dataloader.py

--- vit/dataloader.py
+++ vit/dataloader.py
@@ -1,7 +1,8 @@
 from torch.utils.data import DataLoader
 
+""" Returns train and test dataloaders. Accepts training dataset, test dataset, and batchsize """
 
-def get_dataloaders(training_data, test_data, batchsize):
+def get_dataloaders(training_data, test_data, batchsize=128):
     train_dataloader = DataLoader(dataset=training_data, batch_size=batchsize, shuffle=True, pin_memory=True, num_workers=8)
     test_dataloader = DataLoader(dataset=test_data, batch_size=batchsize, shuffle=False)
 
