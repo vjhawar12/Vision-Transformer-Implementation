d5bf9d14db702fa27eb7e2c99d64885144378e21
Vedant Jhawar <89154560+vjhawar12@users.noreply.github.com>
2025-07-12 10:36:06 +0000

    Update transforms.py

--- vit/transforms.py
+++ vit/transforms.py
@@ -1,13 +1,22 @@
 import torchvision
 
+"""
+Returns torchvision transform pipelines for training and testing data.
+
+The training pipeline includes resizing, data augmentation, and normalization.
+The test pipeline includes resizing and normalization only.
+"""
+
 def get_transforms():
+    # Training pre-processing pipeline incorporates image resizing, random horizontal flip, random crop, color jitter, and normalization
     train_transform = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(64),
-        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.Resize(64), # Upscaling CIFAR-10 image from 32 to 64 increases the number of patches.
+        torchvision.transforms.RandomHorizontalFlip(), 
         torchvision.transforms.RandomCrop(64, padding=4),
         torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
         torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.49139968, 0.48215827, 0.44653124), (0.24703233, 0.24348505, 0.26158768)) # normalizing mean helps optims like Adam, scaling ensures uniformity across the inputs
+        torchvision.transforms.Normalize((0.49139968, 0.48215827, 0.44653124), (0.24703233, 0.24348505, 0.26158768)) 
+        # normalizing mean helps optims like Adam, scaling ensures uniformity across the inputs
     ])
 
     test_transform = torchvision.transforms.Compose([
